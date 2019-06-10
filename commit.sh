16a8cf358b2d22c59023a68c00b4d3b4aebce3f5
pill <pill_ing@naver.com>
2019-06-10 21:03:39 +0800

    20190610-2103

--- commit.sh
+++ commit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export LANG=en_US.utf8
 NowDate=$(date +%Y%m%d)-$(date +%H%M) 
-cd /volume1/web/git/pill0206.githube.io
+cd /volume1/web/git/pill0206.github.io
 git add *
 git commit -m $NowDate
 git push
